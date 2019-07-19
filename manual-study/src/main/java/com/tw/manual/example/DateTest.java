4bcbba155c96fa7085a65c29b7fb35e1ecb00a6f
tanwei <tan2390668854@163.com>
2019-07-19 19:31:01 +0800

    commit

---
+++ manual-study/src/main/java/com/tw/manual/example/DateTest.java
@@ -0,0 +1,17 @@
+package com.tw.manual.example;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+/**
+ * @Author: tanwei
+ * @Description:
+ * @Date: Create in 18:34 2019/7/19
+ */
+public class DateTest {
+    public static void main(String[] args) {
+        Instant instant = Instant.now();
+
+        LocalDateTime localDateTime=LocalDateTime.now();
+    }
+}
